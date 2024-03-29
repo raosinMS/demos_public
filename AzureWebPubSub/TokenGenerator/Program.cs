0941bf734606b224a185022207947138365cafe3
raosin_microsoft <81429063+raosinMS@users.noreply.github.com>
2024-03-29 08:40:39 +0100

    Add files via upload

---
+++ AzureWebPubSub/TokenGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure;
+using Azure.Messaging.WebPubSub;
+
+Console.WriteLine("Hello, World!");
+
+
+var serviceClient = new WebPubSubServiceClient(
+        new Uri("https://<endpoint>.webpubsub.azure.com"), 
+        "pubsubhub1", 
+        new AzureKeyCredential("<KEY>"));
+
+
+var url = serviceClient.GetClientAccessUri(userId: "client1");
+url = serviceClient.GetClientAccessUri(expiresAfter: TimeSpan.FromMinutes(5));
+//var url = service.GetClientAccessUri(roles: new string[] { "webpubsub.joinLeaveGroup.group1" });
+Console.WriteLine(url);
